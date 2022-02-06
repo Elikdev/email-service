ad466b007239facece0cbdc5571ae719f0007355
Odutoye Kolade Elijah <koladeodutoye9913@gmail.com>
2022-02-06 17:07:58 +0100

    added params

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const express = require("express")
 const app = express()
 
 
-app.get("/", (req, res) => {
+app.get("/:name", (req, res) => {
+ const {name} =  req.params
  return res.status(200).json({
-  message: "welcome to my server"
+  message: `welcome to my server, ${name ? name : "guest"}`
  })
 })
 
